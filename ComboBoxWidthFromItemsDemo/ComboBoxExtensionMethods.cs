85811e0961836f2407909a9549562d734203f807
V717826 <V717826@V10082433>
2018-09-20 11:46:21 -0500

    Add project files.

---
+++ ComboBoxWidthFromItemsDemo/ComboBoxExtensionMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace ComboBoxWidthFromItemsDemo
+{
+    public static class ComboBoxExtensionMethods
+    {
+        public static void SetWidthFromItems(this ComboBox box)
+        {
+            double comboBoxWidth = 19.0;
+
+            ComboBoxAutomationPeer peer = new ComboBoxAutomationPeer(box);
+            IExpandCollapseProvider provider = (IExpandCollapseProvider)peer.GetPattern(PatternInterface.ExpandCollapse);
+            EventHandler handler = null;
+
+            handler = new EventHandler(delegate
+            {
+                if (box.IsDropDownOpen && box.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
+                {
+                    double width = 0.0;
+
+                    foreach (var item in box.Items)
+                    {
+                        ComboBoxItem boxItem = box.ItemContainerGenerator.ContainerFromItem(item) as ComboBoxItem;
+                        boxItem.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+                        if(boxItem.DesiredSize.Width > width)
+                            width = boxItem.DesiredSize.Width;
+                    }
+                    box.Width = comboBoxWidth + width;
+
+                    box.ItemContainerGenerator.StatusChanged -= handler;
+                    box.DropDownOpened -= handler;
+                    provider.Collapse();
+                }
+            });
+
+            box.ItemContainerGenerator.StatusChanged += handler;
+            box.DropDownOpened += handler;
+            provider.Expand();
+        }
+    }
+}
---
+++ ComboBoxWidthFromItemsDemo/ComboBoxWidthFromItemsBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ComboBoxWidthFromItemsDemo
+{
+    public static class ComboBoxWidthFromItemsBehavior
+    {
+        public static readonly DependencyProperty ComboBoxWidthFromItemsProperty = DependencyProperty.RegisterAttached("ComboBoxWidthFromItems", typeof(bool),
+            typeof(ComboBoxWidthFromItemsBehavior), new UIPropertyMetadata(false, OnComboBoxWidthFromItemsPropertyChanged));
+
+        public static bool GetComboBoxWidthFromItems(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ComboBoxWidthFromItemsProperty);
+        }
+
+        public static void SetComboBoxWidthFromItems(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ComboBoxWidthFromItemsProperty, value);
+        }
+
+        private static void OnComboBoxWidthFromItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox box = d as ComboBox;
+
+            if(box != null)
+            {
+                if((bool)e.NewValue == true)
+                    box.Loaded += OnComboBoxLoaded;
+                else
+                    box.Loaded -= OnComboBoxLoaded;
+            }
+        }
+
+        private static void OnComboBoxLoaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox box = sender as ComboBox;
+            Action action = () => { box.SetWidthFromItems(); };
+            box.Dispatcher.BeginInvoke(action, DispatcherPriority.ContextIdle);
+        }
+    }
+}
